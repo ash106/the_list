c53cb9bc9cbd9bff409611a1aad6ecf5b4722dcf
Alex Howington <alex.howington@gmail.com>
2019-10-15 21:35:16 -0500

    Try webpack for Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,20 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery3")
-require("popper")
-require("bootstrap-sprockets")
-
+import '../application'
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/button'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/index'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/popover'
+import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tab'
+import 'bootstrap/js/dist/toast'
+import 'bootstrap/js/dist/tooltip'
+import 'bootstrap/js/dist/util'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
